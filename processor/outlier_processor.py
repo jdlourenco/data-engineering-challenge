91a267283b7e9f2c5a26ff1e0f6e60dfb2a1cf44
José Lourenço <joseduartelourenco@gmail.com>
2019-02-16 23:36:53 +0000

    add compute_mean and compute_stdev methods working on dict arrays returned by outlier adapter add get_outliers method implementing three-sigma rule

--- processor/outlier_processor.py
+++ processor/outlier_processor.py
@@ -1,17 +1,33 @@
 from processor.abstract_processor import AbstractProcessor
 
+import statistics
+
 class OutlierProcessor(AbstractProcessor):
 
-	PARAMS = ['input_df', 'column']
+	PARAMS = ['input_data', 'column', 'writer']
 
 	def process(self, params):
-
-		[input_df, column] = map(params.get, self.PARAMS)
+		[input_data, column, writer] = map(params.get, self.PARAMS)
 		
-		mean = input_df._get_numeric_data().mean()[column]
-		std  = input_df._get_numeric_data().std()[column]
+		mean    = self.compute_mean(input_data, column)
+		std_dev = self.compute_stdev(input_data, column)
 
-		three_sigma = mean + 3 * std
+		print(f'3sigma outlier detector on field=\'{column}\': mean={round(mean, 3)} stdev={round(std_dev, 3)}')
 
-		outliers = input_df[input_df[column] > three_sigma]
-		print(outliers)
+		for outlier in self.get_outliers(input_data, column, mean, std_dev):
+			writer.write(outlier)
+
+	def compute_mean(self, data, column):
+		mean = statistics.mean(map(lambda v: v[column], data))
+
+		return mean
+
+	def compute_stdev(self, data, column):
+		std_dev = statistics.stdev(map(lambda v: v[column], data))
+
+		return std_dev
+
+	def get_outliers(self, data, column, mean, std_dev):
+		three_sigma = mean + 3 * std_dev
+
+		return [ entry for entry in data if entry[column] > three_sigma ]
