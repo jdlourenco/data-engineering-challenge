f4b157781c7f0d0b2dd4e7d59d9d850ad2c869ce
José Lourenço <joseduartelourenco@gmail.com>
2019-02-16 23:24:02 +0000

    remove custom code for converting datetime objects to string add datetime json encoder for dumping jsons containing datetime objects

--- writer/json_stdout_writer.py
+++ writer/json_stdout_writer.py
@@ -1,17 +1,16 @@
 from writer.abstract_writer import AbstractWriter
 
-import datetime
 import json
+from datetime import date, datetime
 
 class JsonStdoutWriter(AbstractWriter):
 
-	TIME_FMT = '%Y-%m-%d %H:%M:%S'
-
 	def write(self, data):
-		data_t = {
-			k: self.datetime_to_s(v) if type(v) == datetime.datetime else v for k, v in data.items()
-		}
-		print(json.dumps(data_t))
+		print(json.dumps(data, default=DateTimeEncoder.default))
 
-	def datetime_to_s(self, datetime_obj):
-		return datetime_obj.strftime(self.TIME_FMT)
+class DateTimeEncoder(json.JSONEncoder):
+    def default(obj):
+        if isinstance(obj, (datetime, date)):
+            return obj.isoformat()
+
+        return json.JSONEncoder.default(self, obj)
