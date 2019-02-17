effe95fb58776e7ce6078d3515a32850c260807f
José Lourenço <joseduartelourenco@gmail.com>
2019-02-17 23:56:02 +0000

    add ~ 1% outlier generation

--- generate_entries.py
+++ generate_entries.py
@@ -4,20 +4,33 @@ import json
 import datetime
 import sys
 
+NENTRIES    = 1_000_000
+
 LANGUAGES   = ['en', 'pt', 'fr', 'es', 'de']
 CLIENT_NAME = ['easyjet', 'booking']
 TIME_FMT    = '%Y-%m-%d %H:%M:%S.%f'
-NENTRIES    = 1_000_000
+EVENT_NAME  = ['translation_requested', 'translation_delivered']
 
-n_entries = int(sys.argv[1]) or NENTRIES
+n_entries = int(sys.argv[1]) if len(sys.argv) >= 2 else NENTRIES
 
 f = open(f'./random_{n_entries}.json', "w")
 
 ts = datetime.datetime.now() - datetime.timedelta(days=365)
 
+has_outlier = False
+outlier_ith = random.randint(0,99)
+
 for i in range(n_entries):
 	s_lan = random.choice(LANGUAGES)
 	t_lan = random.choice([l for l in LANGUAGES if l != s_lan])
+
+	is_outlier = i  % 100 == outlier_ith
+
+	if not has_outlier and is_outlier:
+		has_outlier = True
+
+	if not has_outlier and i == n_entries - 1:
+		is_outlier = True
 
 	entry = { 
 		"timestamp":       ts.strftime(TIME_FMT),
@@ -25,9 +38,9 @@ for i in range(n_entries):
 		"source_language": s_lan,
 		"target_language": t_lan,
 		"client_name":     random.choice(CLIENT_NAME),
-		"event_name":      "translation_requested",
+		"event_name":      random.choice(EVENT_NAME),
 		"nr_words":        random.randint(1,1000),
-		"duration":        random.randint(1,60)
+		"duration":        random.randint(1,60) if not is_outlier else random.randint(1000,6000)
 	}
 
 	f.write(f'{json.dumps(entry)}\n')
