02853354c0b71ea8e0f27d240bd8e3a531bff1ec
José Lourenço <joseduartelourenco@gmail.com>
2019-02-17 21:23:08 +0000

    change raise InvalidWindowSizeException when window_size is not an int or is less than 1 add private __valid_el method for validating elements added to window change raise InvalidElementException when element added to window is not valid

--- processor/moving_average_processor.py
+++ processor/moving_average_processor.py
@@ -5,11 +5,11 @@ import datetime
 
 class MovingAverageProcessor(AbstractProcessor):
 
-	PARAMS    = ['input_data', 'window_size', 'column', 'writer']
+	PARAMS    = ['input_data', 'window_size', 'event_name', 'writer']
 	EMPTY_ROW = {'sum': 0, 'count':0}
 
 	def process(self, params):
-		[ input_data, window_size, column, writer ] = map(params.get, self.PARAMS)
+		[ input_data, window_size, event_name, writer ] = map(params.get, self.PARAMS)
 
 		window = self.create_window(window_size)
 
@@ -20,7 +20,7 @@ class MovingAverageProcessor(AbstractProcessor):
 
 			current_ts = row['timestamp'] + datetime.timedelta(minutes=1)
 
-			writer.write({ 'date': current_ts, f'avg_last{window_size}m_{column}': moving_average})
+			writer.write({ 'date': current_ts, f'average_{event_name}': moving_average})
 
 		# fill in empty data windows while moving average is not zero
 		while True:
@@ -32,12 +32,18 @@ class MovingAverageProcessor(AbstractProcessor):
 
 			current_ts += datetime.timedelta(minutes=1)
 
-			writer.write({ 'date': current_ts, f'avg_last{window_size}m_{column}': moving_average})
+			writer.write({ 'date': current_ts, f'average_{event_name}': moving_average})
 
 	def create_window(self, window_size):
+		if not type(window_size) == int or window_size < 1:
+			raise InvalidWindowSizeException(f'Invalid window size: {window_size}')
+
 		return collections.deque(maxlen=window_size)
 
 	def add_to_window(self, window, el):
+		if not self.__valid_el(el):
+			raise InvalidElementException(f'Invalid element {el}')
+
 		window.append(el)
 
 		return window
@@ -47,3 +53,19 @@ class MovingAverageProcessor(AbstractProcessor):
 		acc_count = sum([x['count'] for x in window])
 
 		return 0 if acc_count == 0 else acc_sum / acc_count
+
+	def __valid_el(self, el):
+		if not type(el) == dict:
+			return False
+
+		for k in self.EMPTY_ROW.keys():
+			if not k in el.keys():
+				return False
+
+		return True
+
+class InvalidWindowSizeException(Exception):
+	pass
+
+class InvalidElementException(Exception):
+	pass
