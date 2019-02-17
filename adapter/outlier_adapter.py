f955adcf09a0d4c633b34577ac420229673a7a5d
José Lourenço <joseduartelourenco@gmail.com>
2019-02-17 03:32:44 +0000

    remove unused methods

--- adapter/outlier_adapter.py
+++ adapter/outlier_adapter.py
@@ -10,15 +10,3 @@ class OutlierAdapter(AbstractAdapter):
 		data_frame = data_frame.to_dict('records')
 
 		return data_frame
-
-	def compute_counts(self, data_frame):
-		counts = data_frame.resample('1Min').count()
-		counts.columns = ['count']
-
-		return counts
-
-	def compute_sums(self, data_frame):
-		sums = data_frame.resample('1Min').sum()
-		sums.columns = ['sum']
-
-		return sums
