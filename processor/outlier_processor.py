2f7e2d70065ca19aaf9dd0308be00077c19ed5e3
José Lourenço <joseduartelourenco@gmail.com>
2019-02-17 21:26:04 +0000

    change replace column param with event_name remove column arg from compute_mean, compute_stdev and get_outliers methods change always use  to look for outliers

--- processor/outlier_processor.py
+++ processor/outlier_processor.py
@@ -4,30 +4,30 @@ import statistics
 
 class OutlierProcessor(AbstractProcessor):
 
-	PARAMS = ['input_data', 'column', 'writer']
+	PARAMS = ['input_data', 'event_name', 'writer']
 
 	def process(self, params):
-		[input_data, column, writer] = map(params.get, self.PARAMS)
+		[input_data, event_name, writer] = map(params.get, self.PARAMS)
 		
-		mean    = self.compute_mean(input_data, column)
-		std_dev = self.compute_stdev(input_data, column)
+		mean    = self.compute_mean(input_data)
+		std_dev = self.compute_stdev(input_data)
 
-		print(f'3sigma outlier detector on field=\'{column}\': mean={round(mean, 3)} stdev={round(std_dev, 3)}')
+		print(f'3sigma outlier detector on event_name=\'{event_name}\': mean={round(mean, 3)} stdev={round(std_dev, 3)}')
 
-		for outlier in self.get_outliers(input_data, column, mean, std_dev):
+		for outlier in self.get_outliers(input_data, mean, std_dev):
 			writer.write(outlier)
 
-	def compute_mean(self, data, column):
-		mean = statistics.mean(map(lambda v: v[column], data))
+	def compute_mean(self, data):
+		mean = statistics.mean(map(lambda v: v['duration'], data))
 
 		return float(mean)
 
-	def compute_stdev(self, data, column):
-		std_dev = statistics.stdev(map(lambda v: v[column], data))
+	def compute_stdev(self, data):
+		std_dev = statistics.stdev(map(lambda v: v['duration'], data))
 
 		return float(std_dev)
 
-	def get_outliers(self, data, column, mean, std_dev):
+	def get_outliers(self, data, mean, std_dev):
 		three_sigma = mean + 3 * std_dev
 
-		return [ entry for entry in data if entry[column] > three_sigma ]
+		return [ entry for entry in data if entry['duration'] > three_sigma ]
