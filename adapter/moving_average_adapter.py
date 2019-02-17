4a467d7f9daa4e77bdcebeb11276619860c1bba0
José Lourenço <joseduartelourenco@gmail.com>
2019-02-17 03:34:50 +0000

    change move input dataframe column filtering and indexing into compute_counts and compute_sums

--- adapter/moving_average_adapter.py
+++ adapter/moving_average_adapter.py
@@ -7,23 +7,21 @@ class MovingAverageAdapter(AbstractAdapter):
 	def adapt(self, params):
 		[data_frame, column] = map(params.get, self.PARAMS)
 
-		data_frame = data_frame[['timestamp', column]].set_index('timestamp')
-
-		counts = self.compute_counts(data_frame)
-		sums   = self.compute_sums(data_frame)
+		counts = self.compute_counts(data_frame, column)
+		sums   = self.compute_sums(data_frame, column)
 
 		counts_sums = sums.merge(counts, on='timestamp').reset_index().to_dict('records')
 
 		return counts_sums
 
-	def compute_counts(self, data_frame):
-		counts = data_frame.resample('1Min').count()
+	def compute_counts(self, data_frame, column):
+		counts = data_frame[['timestamp', column]].set_index('timestamp').resample('1Min').count()
 		counts.columns = ['count']
 
 		return counts
 
-	def compute_sums(self, data_frame):
-		sums = data_frame.resample('1Min').sum()
+	def compute_sums(self, data_frame, column):
+		sums = data_frame[['timestamp', column]].set_index('timestamp').resample('1Min').sum()
 		sums.columns = ['sum']
 
 		return sums
